15af848368e923e7ebde05656ce5fe3ae7c519f1
mohdanas1234 <anas@ag4444>
2024-06-22 13:59:54 +0530

    update

---
+++ src/assets/SliderSetting.jsx
@@ -0,0 +1,18 @@
+export const SliderSetting = {
+  slidesPerView: 2,
+  spaceBetween: 20,
+  breakpoints: {
+    480: {
+      slidesPerView: 2,
+    },
+    600: {
+      slidesPerView: 2,
+    },
+    750: {
+      slidesPerView: 3,
+    },
+    1100: {
+      slidesPerView: 4,
+    },
+  },
+};
