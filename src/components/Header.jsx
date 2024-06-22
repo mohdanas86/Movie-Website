cbe17f71d36ad17c314b756c264151747655bf72
mohdanas1234 <anas@ag4444>
2024-06-22 21:15:48 +0530

    update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -47,25 +47,6 @@ const Header = () => {
           </button>
         </div>
       </div>
-      
-      <div className={`z-10 w-full h-full bg-[rgba(0,0,0,0.500)] fixed top-0 overflow-hidden ${!show ? "hidden" : "block"}`} onClick={toggleMenu}>
-        <div className="w-[60%] md:w-[33.3%] h-full bg-white flex flex-col justify-start items-start text-black py-4 px-4 gap-4 relative" onClick={(e) => e.stopPropagation()}>
-          <div className="w-full flex justify-between items-center">
-            <a href="/" className="text-2xl font-semibold">Stomilar</a>
-          </div>
-          <div className="flex justify-center items-start flex-col w-full gap-2">
-            {list.map((v, i) => (
-              <span 
-                className="text-lg font-semibold text-slate-600" 
-                key={i} 
-                onClick={() => categoryClick(v.name)}
-              >
-                <span className="text-blue-600">#</span> {v.name}
-              </span>
-            ))}
-          </div>
-        </div>
-      </div>
     </>
   );
 }
